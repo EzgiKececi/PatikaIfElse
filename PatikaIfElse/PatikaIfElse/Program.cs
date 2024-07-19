ea9f23a4ae08d4e70f61dad945022a68132d04ff
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-07-19 10:26:21 +0300

    Add files via upload

---
+++ PatikaIfElse/PatikaIfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Lütfen bir sayı giriniz:");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if (sayi == 10) // Birinci karar yapısı ile girilen sayının 10'a eşitliği ile büyük ya da küçük olma durumuna bakılacaktır
+{
+    Console.WriteLine("Girdiğiniz sayı 10'a eşittir.");
+}
+else if  (sayi <= 10)
+{
+    Console.WriteLine("Girdiğiniz sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı 10'dan büyüktür.");
+}
+
+if (sayi %2 == 0) // ikinci karar yapısı ile girilen sayının tek mi çift mi olduğuna bakılacaktır
+{
+    Console.WriteLine("Girdiğinz sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı tektir.");
+}
